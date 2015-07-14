90b55c196f2bb94275dec8aded5b2665f256e9d8
kuku <279563126@qq.com>
2015-07-14 20:50:29 +0800

    代码提交

--- appClientModule/com/lprclient/core/action/ExternalClientAction.java
+++ appClientModule/com/lprclient/core/action/ExternalClientAction.java
@@ -2,6 +2,8 @@ package com.lprclient.core.action;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JButton;
 
@@ -14,6 +16,8 @@ import com.lprclient.core.view.panel.RightPanel;
  * @version V1.0    
  */
 public class ExternalClientAction extends BaseAction {
+	
+	private static Process process = null;
 	
 	/**
 	 * 目录点击后跳转
@@ -32,6 +36,7 @@ public class ExternalClientAction extends BaseAction {
 		right.removeAll();
 		right.add(getNavLabel());
 		right.add(getStartBtn());
+		right.add(getStopBtn());
 		right.repaint();
 	}
 	
@@ -42,10 +47,34 @@ public class ExternalClientAction extends BaseAction {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(1111);
+				try {
+					String[] strs = System.getProperty("java.class.path").split(";");
+					System.out.println(strs[0]);
+					String filePath = strs[0] + "\\external\\lab";
+					System.out.println(filePath);
+					process = Runtime.getRuntime().exec(filePath + "\\load.exe", null, new File(filePath));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		});
 		return startBtn;
+	}
+	
+	private JButton getStopBtn() {
+		JButton stopBtn = new JButton("停止");
+		stopBtn.setBounds(450, 250, 80, 20);
+		stopBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (null != process) {
+					process.destroy();
+				}
+			}
+		});
+		return stopBtn;
 	}
 
 }
